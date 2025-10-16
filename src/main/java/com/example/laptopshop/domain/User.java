067466cf8b1ae93d8c1cfb5f54520958f2dcf3ae
duongbean <bean272003@gmail.com>
2025-10-16 14:27:34 +0900

    association

--- src/main/java/com/example/laptopshop/domain/User.java
+++ src/main/java/com/example/laptopshop/domain/User.java
@@ -4,8 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -16,6 +22,16 @@ public class User {
     private String address;
     private String phone;
     private String avatar;
+
+    // roleId
+    // userId;
+    // user many - one role
+    @ManyToOne
+    @JoinColumn(name = "role_id")
+    private Role role;
+
+    @OneToMany(mappedBy = "user")
+    private List<Order> orders;
 
     public long getId() {
         return id;
