11689548373c926a21a0f8a9d71e97e893bd33c1
duongbean <bean272003@gmail.com>
2025-10-16 13:43:29 +0900

    first commit

---
+++ src/main/java/com/example/laptopshop/service/UserService.java
@@ -0,0 +1,41 @@
+package com.example.laptopshop.service;
+
+import java.util.*;
+import org.springframework.stereotype.Service;
+
+import com.example.laptopshop.repository.UserRepository;
+import com.example.laptopshop.domain.User;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public String handleHello() {
+        return "Hello from Service";
+    }
+
+    public List<User> getAllUser() {
+        return this.userRepository.findAll();
+    }
+
+    public List<User> getAllUserByEmaEmail(String email) {
+        return this.userRepository.findOneByEmail(email);
+    }
+
+    public User handleSaveUser(User user) {
+        return this.userRepository.save(user);
+    }
+
+    public User getUserById(long id) {
+        return this.userRepository.findById(id);
+    }
+
+    public void deleteAUser(long id) {
+        this.userRepository.deleteById(id);
+    }
+
+}
